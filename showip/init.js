6cece64595a9744800469faaf1bdf199457a15de
AceP1983 <no@way.net>
2014-10-02 23:29:16 -0500

    minor fix

--- showip/init.js
+++ showip/init.js
@@ -6,7 +6,7 @@ if (plugin.enabled)
                         "<td class='sthdr'>IP:</td>"+
                         "<td class='stval' id='ip-text'></td>") ).get(0),3);
                 $("#ip-text").text(this.ip);
-				$("#ip-td").prop("title", 'WAN: '+this.ip+'\nLAN: '+this.ip);
+				$("#ip-td").prop("title", 'WAN: '+this.ip+'\nLAN: '+this.lan_ip);
 }
 
 plugin.onRemove = function()
