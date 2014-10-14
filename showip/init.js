323ef5c523031e835f7af9928b4e61a031699991
AceP1983 <no@way.net>
2014-10-14 00:55:50 -0500

    minor fixes
    Minor CSS fixes

--- showip/init.js
+++ showip/init.js
@@ -2,8 +2,8 @@ plugin.loadMainCSS();
 plugin.checkIP = function() {
 if (plugin.enabled)
         plugin.addPaneToStatusbar("ip-td",$("<div>").attr("id","ip-holder").
-                append( $("<div>").attr("id","ip-cont").html(
-                        "<td class='sthdr'>IP:</td>"+
+                append( $("<div>").attr("align","center").attr("id","ip-cont").html(
+                        "<td class='sthdr'>IP:&nbsp</td>"+
                         "<td class='stval' id='ip-text'></td>") ).get(0),3);
                 $("#ip-text").text(this.ip);
 				$("#ip-td").prop("title", 'WAN: '+this.ip+'\nLAN: '+this.lan_ip);
